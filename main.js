2c6794db939f54afc5f6af5752777f6b84f71665
Adler Faulkner <aif28@cornell.edu>
2018-02-15 01:28:30 -0500

    card transitions working

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var projects = [
   {
     title: 'Wire Bender',
     card_image: 'img/wire-bender.gif',
-    link: 'light-to-sound'
+    link: 'wire-bender'
   },
   {
     title: 'Light 👉🏽 Sound',
@@ -51,17 +51,87 @@ var projects = [
   },
 ];
 
-var cardTemplate, projectList;
+var projectTemplate, projectList, url, idx, pageLink, cardTemplate;
 
-loadProject = function(i) {
+loadProject = function() {
+  var $this = $(this);
+  // Get the link for this card
+  var thisPageLink = $this.data('link')
+  // Update the browser url if the current pageLink is not this page's link
+  if (pageLink != thisPageLink) {
+    history.pushState({isValid:true}, "", url + "#" + thisPageLink);
+    pageLink = thisPageLink;
+  }
+  initialPosition = $this.offset()
+  var newCard = cardTemplate.clone()
+    .removeClass('template');
+  $('body').append(newCard)
+
+  var cardPadding = newCard.find('.card-padding-wrapper')
+    .css({
+      'padding':0,
+      'left': initialPosition.left,
+      'top': initialPosition.top,
+      'height': $this.find('img').outerHeight(),
+      'width': $this.find('img').outerWidth(),
+      'margin-left': 0,
+      'margin-right': 0,
+      'opacity': 0.2
+    });
+  var card = cardPadding.find('.card')
+    .css({
+      '-webkit-border-radius': '7px',
+      '-moz-border-radius': '7px',
+      '-ms-border-radius': '7px',
+      'border-radius': '7px'
+    });
+  var mainCardPaddingWidth = $('#main-card .card-padding-wrapper').width()
+  var margin = (window.innerWidth - Math.min(window.innerWidth, 1300))/2
+  cardPadding.css({
+    'left': 0,
+    'top': 0,
+    'padding':45,
+    'height': window.innerHeight,
+    'width': Math.min(window.innerWidth, 1300),
+    'margin-left': margin,
+    'margin-right': margin,
+    'opacity': 1
+  });
+  var scaleValue = 0.95;
+  card.css({
+    '-webkit-border-radius': '35px',
+    '-moz-border-radius': '35px',
+    '-ms-border-radius': '35px',
+    'border-radius': '35px'
+  });
+  setTimeout( function() {
+    $('#main-card .card-padding-wrapper').css({
+      'left' : -(mainCardPaddingWidth*0.08),
+      '-webkit-transform' : 'scale(' + scaleValue + ')',
+      '-moz-transform'    : 'scale(' + scaleValue + ')',
+      '-ms-transform'     : 'scale(' + scaleValue + ')',
+      '-o-transform'      : 'scale(' + scaleValue + ')',
+      'transform'         : 'scale(' + scaleValue + ')',
+      '-webkit-filter' : 'blur(4px)',
+      'filter': 'blur(4px)'
+    }).addClass('darken')
+  }, 100);
+}
+
+loadProjectContainer = function(i) {
   if (projects.length >= i+1) {
-    project = projects[i];
-    var card = cardTemplate.clone().removeClass('template');
+    var project = projects[i];
+    var card = projectTemplate.clone()
+      .removeClass('template')
+      .data(project)
+      .attr(project)
     var img = card.find('img');
     img.data('src', project['card_image']);
     card.find('p').html(project['title']);
     projectList.append(card);
     loadImageHelper(img.get(0));
+  } else {
+    loadCardFromUrl()
   }
 }
 
@@ -70,10 +140,12 @@ loadImageHelper = function(image) {
   var downloadingImage = new Image();
   downloadingImage.onload = function() {
     image.src = this.src;
-    projectContainer = $image.closest('.project-container');
+    var projectContainer = $image.closest('.project-container');
     if (projectContainer.length > 0) {
-      projectContainer.css('opacity', 1)
-      loadProject(projectContainer.index() + 1);
+      projectContainer.css('opacity', 1);
+      index = projectContainer.index()
+      loadProjectContainer(index + 1);
+      projectContainer.click(loadProject);
     }
   }
   downloadingImage.onerror = function() {
@@ -91,8 +163,19 @@ loadImages = function() {
   });
 }
 
+loadCardFromUrl = function() {
+  url = window.location.href;
+  idx = url.indexOf("#");
+  if (idx != -1) {
+    pageLink = url.substring(idx+1);
+    url = url.substring(0, idx);
+    loadProject.call($(".project-container[link='" + pageLink + "']"))
+  }
+}
+
 window.onload = function() {
-  cardTemplate = $('.project-container.template');
+  projectTemplate = $('.project-container.template');
   projectList = $('#main-card .card-body');
-  loadProject(0)
+  cardTemplate = $('.card-scroll-wrapper.template');
+  loadProjectContainer(0);
 };
